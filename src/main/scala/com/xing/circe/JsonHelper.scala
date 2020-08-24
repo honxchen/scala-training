fd8b1f7e84ccbdcc4f251ad02b5bc37ece64d2e9
honxchen <honxchen@thoughtworks.com>
2020-08-24 22:31:34 +0800

    remove answer

--- src/main/scala/com/xing/circe/JsonHelper.scala
+++ src/main/scala/com/xing/circe/JsonHelper.scala
@@ -1,32 +1,17 @@
 package com.xing.circe
 
-import io.circe.Encoder.encodeMap
-import io.circe.{Encoder, Json}
-import io.circe.parser._
-import io.circe.syntax._
-import io.circe.generic.auto._
-import org.http4s.{Header, Headers}
+import org.http4s.Headers
 
 case class Event(name: String)
 case class HttpEvent(url: String, headers: Headers)
 
 object JsonHelper {
 
-//  implicit val encodeFoo: Encoder[Headers] = new Encoder[Headers] {
-//    final def apply(a: Headers): Json = a.toList.map(h => (h.name.toString(), h.value)).toMap.asJson
-//  }
+  def string2Json(string: String) = ???
 
-//  import io.circe.KeyEncoder.encodeKeyString
-//  import io.circe.Encoder.encodeString
-//  implicit val encodeFoo: Encoder[Headers] = encodeMap(encodeKeyString, encodeString).contramap(a => a.toList.map(h => (h.name.toString(), h.value)).toMap)
+  def liststring2Json(list: List[String]) = ???
 
-//  implicit def encodeFoo(implicit mapEncoder: Encoder[Map[String, String]]): Encoder[Headers] = mapEncoder.contramap(a => a.toList.map(h => (h.name.toString(), h.value)).toMap)
+  def event2Json(event: Event) = ???
 
-  def string2Json(string: String) = parse(string).right.get
-
-  def liststring2Json(list: List[String]) = list.asJson
-
-  def event2Json(event: Event) = event.asJson
-
-  def headerEvent2Json(headerEvent: HttpEvent) = headerEvent.asJson
+  def headerEvent2Json(headerEvent: HttpEvent) = ???
 }
