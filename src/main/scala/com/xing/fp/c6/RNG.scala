50283d311a8bcaecb2810a518b4183a179cdb8b5
honxchen <honxchen@thoughtworks.com>
2020-08-25 23:03:23 +0800

    non negative int

--- src/main/scala/com/xing/fp/c6/RNG.scala
+++ src/main/scala/com/xing/fp/c6/RNG.scala
@@ -4,11 +4,19 @@ trait RNG {
   def nextInt: (Int, RNG)
 }
 
-case class SimpleRNG(seed: Long) extends RNG {
-  override def nextInt: (Int, RNG) = {
-    val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
-    val nextRNG = SimpleRNG(newSeed)
-    val n = (newSeed >>> 16).toInt
-    (n, nextRNG)
+object RNG {
+
+  case class SimpleRNG(seed: Long) extends RNG {
+    override def nextInt: (Int, RNG) = {
+      val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+      val nextRNG = SimpleRNG(newSeed)
+      val n = (newSeed >>> 16).toInt
+      (n, nextRNG)
+    }
+  }
+
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val (i, nextRng) = rng.nextInt
+    (if (i < 0) -(i + 1) else i, nextRng)
   }
 }
--- src/test/scala/com/xing/fp/c6/SimpleRNGTest.scala
+++ src/test/scala/com/xing/fp/c6/SimpleRNGTest.scala
@@ -1,5 +1,7 @@
 package com.xing.fp.c6
 
+import com.xing.fp.c6.RNG.nonNegativeInt
+import com.xing.fp.c6.RNG._
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers
 
@@ -12,5 +14,12 @@ class SimpleRNGTest extends AnyWordSpec with Matchers{
       n1 shouldEqual 384748
       rng2 shouldEqual SimpleRNG(25214903928L)
     }
-  }
+
+    "non negative" in {
+      val rng = SimpleRNG(1)
+      val (n1, rng2) = nonNegativeInt(rng)
+      n1 shouldEqual 384748
+      rng2 shouldEqual SimpleRNG(25214903928L)
+    }
+   }
 }
